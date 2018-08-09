d5f5aeb6a6914046b731f135c5c07eee593cb066
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-09 13:30:09 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,11 +4,19 @@ rainbow_colors = [
   "light_cyan"]
 
 def change_rainbow_colors
+  
+  
+  
+  
+  
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
 end
+
+change_rainbow_colors
+
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 end
 
-change_rainbow_colors
+add_colors
